eb5f08d80375e94285165b7e976f5b51c4f55feb
Radu Vrinceanu <radu.vrinceanu@ivfuture.uk>
2021-11-05 17:40:21 +0200

    hotfix: refactored

--- routes/organization.php
+++ routes/organization.php
@@ -3,24 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrganizationController;
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/organizations', [OrganizationController::class, 'index'])
-        ->middleware('admin')
         ->name('organizations.index');
 
     Route::get('/organizations/{organization}', [OrganizationController::class, 'edit'])
-        ->middleware('admin')
         ->name('organizations.edit');
 
     Route::post('/organizations', [OrganizationController::class, 'store'])
-        ->middleware('admin')
         ->name('organizations.store');
 
     Route::patch('/organizations/{organization}', [OrganizationController::class, 'update'])
-        ->middleware('admin')
         ->name('organizations.update');
 
     Route::delete('/organizations/{organization}', [OrganizationController::class, 'delete'])
-        ->middleware('admin')
         ->name('organizations.delete');
 });
